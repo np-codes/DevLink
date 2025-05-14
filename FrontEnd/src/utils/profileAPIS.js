bb31f2df3c8e6db7468468f4c39db78a54041de0
Nisarg Patel <nish632002.patel@gmail.com>
2025-05-14 18:13:38 -0400

    Organized APIS

--- FrontEnd/src/utils/profileAPIS.js
+++ FrontEnd/src/utils/profileAPIS.js
@@ -1,8 +1,6 @@
 import { BASE_URL } from "./constants";
-import { addUser, removeUser, updateUser } from "./userSlice";
-import axios, { AxiosError } from "axios";
-import { persistor } from './appStore';
-import { addFeed } from "./feedSlice";
+import { addUser, updateUser } from "./userSlice";
+import axios from "axios";
 import toast from "react-hot-toast";
 
 export const Edit_Profile_API = async({
@@ -44,36 +42,4 @@ export const Fetch_User_Profile_API = async({dispatch, navigate}) => {
     }
 }
 
-export const Login_API = async({loginInfo, dispatch, navigate, setErrorMessage}) => {
-	try{
-      const res = await axios.post(BASE_URL + "/login", 
-        loginInfo,
-        { withCredentials : true }
-      )
-      const data = res?.data?.data;
-      dispatch(addUser(data));
-		toast.success(res.data.message)
-      return navigate("/feed");
-    } catch (err) {
-      setErrorMessage(err?.response?.data?.message || "Something Went Wrong.");
-      throw new Error( "Error Occured : ", err?.response?.data?.message || "Something Went Wrong.");
-    }
-}
-
-export const Logout_API = async ({dispatch}) => {
-	try {
-		dispatch(removeUser());
-      const res = await axios.post(
-        BASE_URL + "/logout",
-        {},
-        {
-         	withCredentials: true,
-        }
-      );
-      persistor.purge();
-		toast.success(res.data.message)
-    } catch (err) {
-      throw new Error("Something Went Wrong : " + err.message);
-    }
-}
 
---
+++ FrontEnd/src/utils/userAPIS.js
@@ -0,0 +1,30 @@
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
+import { addFeed } from "./feedSlice";
+import { addConnections } from "./connectionSlice";
+
+export const User_Feed_API = async ({dispatch}) => {
+    try{
+      const res = await axios.get(BASE_URL + "/user/feed", {
+        withCredentials : true
+      });
+      const data = res.data?.data;
+      dispatch(addFeed(data));
+   	} catch (err) {
+      throw new Error("Error Occured : ", err?.response?.data?.message || "Something Went Wrong.");
+   	}
+}
+
+export const User_Connections_API = async({dispatch}) => {
+    try{
+      const res = await axios.get(BASE_URL + "/user/connections", {
+        withCredentials : true
+      });
+			const data = res.data?.data;
+			console.log(data)
+      dispatch(addConnections(data))
+    }
+    catch (err) {
+      throw new Error("Error Occured : ", err?.response?.data?.message || "Something Went Wrong.");
+   }
+}
