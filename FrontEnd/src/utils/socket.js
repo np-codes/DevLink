b7802f416fa1bd1d8fece39624db9373818fc5fe
Nisarg Patel <nish632002.patel@gmail.com>
2025-07-04 08:25:04 -0400

    Socket Bug Fixing

--- FrontEnd/src/utils/socket.js
+++ FrontEnd/src/utils/socket.js
@@ -3,7 +3,7 @@ import { BASE_URL } from "./constants";
 
 export const createSocketConnection = (token) => {
     return io(BASE_URL, {
-        path: "/api/socket.io",
+        path: "socket.io",
         auth: {
             token,
         },
